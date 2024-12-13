23d346794414e2de948990623c5dd4e8f4542ba6
Charles <cnavarro3196@protonmail.com>
2024-12-13 01:02:27 -0600

    feat[app]: remove all code

--- 1/front-end/src/app/deadline/deadline.component.ts
+++ 1/front-end/src/app/deadline/deadline.component.ts
@@ -12,19 +12,7 @@ import { take, tap } from 'rxjs';
 })
 export class DeadlineComponent {
   currentDate = signal(new Date());
-  secondsLeft$ = this.deadlineService.getCountdown().pipe(
-    tap(secondsLeft => {
-      if(secondsLeft === 0){
-        console.log('Countdown Complete!')
-      }
-    })
-  );
+  secondsLeft$ = this.deadlineService.getCountdown();
   deadlineDate = this.deadlineService.getDeadlineDate().pipe(take(2));
   constructor(private readonly deadlineService: DeadlineService) { }
-
-  resetDeadline(){
-    this.secondsLeft$.subscribe((secondsLeft) => {
-      console.log('secondsLeft', secondsLeft);
-    })
-  }
 }
